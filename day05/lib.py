87a997da41edb402f32bc0277f4e8a563e9c2960
Peter van der Heijden <peter@vanderheijden.name>
2021-12-01 22:24:29 +0100

    Make the algorithm a little faster by updating the prefix.

--- day05/lib.py
+++ day05/lib.py
@@ -3,8 +3,11 @@ import hashlib
 def part1(code):
     password = ""
     index = 0
+    prefix = hashlib.md5(code.encode('utf-8'))
     while len(password) < 8:
-        md5 = hashlib.md5((code + str(index)).encode('utf-8')).hexdigest()
+        hash = prefix.copy()
+        hash.update(str(index).encode('utf-8'))
+        md5 = hash.hexdigest()
         if md5[:5] == "00000":
             password += md5[5]
             print(password)
@@ -15,8 +18,11 @@ def part1(code):
 def part2(code):
     password = ["_"] * 8
     index = 0
+    prefix = hashlib.md5(code.encode('utf-8'))
     while "_" in password:
-        md5 = hashlib.md5((code + str(index)).encode('utf-8')).hexdigest()
+        hash = prefix.copy()
+        hash.update(str(index).encode('utf-8'))
+        md5 = hash.hexdigest()
         if md5[:5] == "00000" and md5[5] in "01234567":
             pos = int(md5[5])
             if password[pos] == "_":
--- day05/main.py
+++ day05/main.py
@@ -1,6 +1,7 @@
 import csv
 import pyperclip
 from lib import part1, part2
+from datetime import datetime
 
 
 def read_rows(**kwargs):
@@ -17,10 +18,17 @@ def clip(x):
 
 
 row = read_rows()
+t0 = datetime.now()
 x = part1(row)
+t1 = datetime.now()
 print(f"Part 1: {x}")
 clip(x)
 
+t2 = datetime.now()
 x = part2(row)
+t3 = datetime.now()
 print(f"Part 2: {x}")
+print(t1 - t0)
+print(t3 - t2)
+print((t1 - t0) + (t3 - t2))
 clip(x)
